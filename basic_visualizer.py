f0a6cf94e8167ece97d976af6420cd0288cf5ed3
Stephen Smith <stephenlehanesmith@gmail.com>
2022-01-04 12:29:39 +0800

    import test data and check conversion of csv to DataFrame

--- basic_visualizer.py
+++ basic_visualizer.py
@@ -2,3 +2,11 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# import test data using pandas .read_csv()
+semester_one_begin = pd.read_csv(r"progress_test_data_begin_sem_1.csv")
+semester_one_end = pd.read_csv(r"progress_test_data_end_sem_1.csv")
+
+# check that there were no problems converting csv to dataframes
+print(semester_one_begin.head())
+print(semester_one_end.head())
