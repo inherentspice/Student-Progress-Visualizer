d5bc85aff77ebc7dc5c6d134bfbb50fef69ccab4
Stephen Smith <stephenlehanesmith@gmail.com>
2022-01-06 19:21:14 +0800

    added barplot that combines and compares semester one begin with semester one end, changed colors and opacity to improve visual comprehension

--- basic_visualizer.py
+++ basic_visualizer.py
@@ -14,6 +14,7 @@ print(semester_one_end.head())
 # don't want the total_score row in the figure, so we drop it from the
 # Dataframe using .drop()
 pd1 = semester_one_begin.drop(semester_one_begin.index[-1])
+pd2 = semester_one_end.drop(semester_one_end.index[-1])
 
 # initialize the figure
 fig, ax = plt.subplots()
@@ -22,17 +23,26 @@ fig, ax = plt.subplots()
 # data to plot on the y-axis, the x-axis title, the y-axis table, the color
 # of the bars, and the title of the graph
 
-def make_bar(axes, x, y, xlabel, ylabel, color, title):
-    ax.bar(x, y, color=color)
-    plt.xticks(rotation=45)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.ylim(0, (max(y)+2))
-    plt.title(label=title)
+# def make_bar(axes, x, y, xlabel, ylabel, color, title):
+#     ax.bar(x, y, color=color)
+#     plt.xticks(rotation=45)
+#     plt.xlabel(xlabel)
+#     plt.ylabel(ylabel)
+#     plt.ylim(0, (max(y)+2))
+#     plt.title(label=title)
+#
+# # call make_bar function with student name on x-axis, score for writing text
+# # on y-axis
+# make_bar(ax, pd1["Name"], pd1["Writing Speed (Two Minutes)"], "Name of student", "Words written in two minutes", "purple", "Writing speed test")
 
-# call make_bar function with student name on x-axis, score for writing text
-# on y-axis
-make_bar(ax, pd1["Name"], pd1["Writing Speed (Two Minutes)"], "Name of student", "Words written in two minutes", "purple", "Writing speed test")
+ax.bar(pd1["Name"], pd1["Speaking"], color='blue')
+plt.xticks(rotation=45)
+ax2 = ax.twinx()
+ax2.bar(pd2["Name"], pd2["Speaking"], color='red', alpha=0.3)
+plt.ylim(0, (max(semester_one_begin["Speaking"])))
+plt.xlabel("Student name")
+plt.ylabel("Speaking score")
+plt.title(label="Speaking Assessment")
 
 # show graph
 plt.show()
