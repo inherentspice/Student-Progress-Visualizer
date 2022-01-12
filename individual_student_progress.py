d9d140a7452cba157ca8160136ad2d8c53a7d539
Stephen Smith <stephenlehanesmith@gmail.com>
2022-01-12 20:39:55 +0800

    merged the two dataframes so they could be used more easily with seaborn

--- individual_student_progress.py
+++ individual_student_progress.py
@@ -1,18 +1,17 @@
-# import packages: pandas to work with tabular data, matplotlib.pyplot to visualize data
+# import packages: pandas to work with tabular data, matplotlib.pyplot to visualize data,
+# seaborn to easily visualize data
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # import test data using pandas .read_csv()
-semester_one_begin = pd.read_csv(r"progress_test_data_begin_sem_1.csv")
-semester_one_end = pd.read_csv(r"progress_test_data_end_sem_1.csv")
-
-# check that there were no problems converting csv to dataframes
-print(semester_one_begin.head())
-print(semester_one_end.head())
+semester_one_begin = pd.read_csv(r"progress_test_data_begin_sem_1.csv", index_col="Name")
+semester_one_end = pd.read_csv(r"progress_test_data_end_sem_1.csv", index_col="Name")
 
 # don't want the total_score row in the figure, so we drop it from the
 # Dataframe using .drop()
 pd1 = semester_one_begin.drop(semester_one_begin.index[-1])
 pd2 = semester_one_end.drop(semester_one_end.index[-1])
 
+merged = pd1.merge(pd2, on="Name", how="outer", suffixes=("_begin", "_end"))
