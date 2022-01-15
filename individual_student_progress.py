9492dcd8b469e55f7bddbde3202b29908ba6d4aa
Stephen Smith <stephenlehanesmith@gmail.com>
2022-01-15 19:30:51 +0800

    Split the merged DataFrame into dataframes for each category

--- individual_student_progress.py
+++ individual_student_progress.py
@@ -14,4 +14,19 @@ semester_one_end = pd.read_csv(r"progress_test_data_end_sem_1.csv", index_col="N
 pd1 = semester_one_begin.drop(semester_one_begin.index[-1])
 pd2 = semester_one_end.drop(semester_one_end.index[-1])
 
+# merge the two dataframes so they can be visualized easily
 merged = pd1.merge(pd2, on="Name", how="outer", suffixes=("_begin", "_end"))
+
+# use .columns to check the suffix names are correct
+print(merged.columns)
+
+# create dataframes seperated into categories
+trans = merged[["Transcription_begin", "Transcription_end"]].copy()
+writing = merged[["Writing Speed (Two Minutes)_begin", "Writing Speed (Two Minutes)_end"]].copy()
+reading = merged[["Reading_begin", "Reading_end"]].copy()
+sight = merged[["Sight Words_begin", "Sight Words_end"]].copy()
+speak = merged[["Speaking_begin", "Speaking_end"]].copy()
+
+#test visualization
+speak.plot.bar()
+plt.show()
