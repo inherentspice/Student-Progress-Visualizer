302a478f009169cb255282538f52ac2541569100
Stephen Smith <stephenlehanesmith@gmail.com>
2022-01-16 19:55:05 +0800

    changed color palletes and colors

--- individual_student_progress.py
+++ individual_student_progress.py
@@ -21,18 +21,18 @@ merged = pd1.merge(pd2, on="Name", how="outer", suffixes=("_begin", "_end"))
 f, ax = plt.subplots(figsize=(6, 15))
 
 # set color palette
-sns.set_color_codes("pastel")
+sns.set_color_codes("deep")
 
 # plot the end of semester one results first
 sns.barplot(x=merged.index, y="Transcription_end", data=merged,
             label="January, 2022", color="b")
 
 # set color of September plot
-sns.set_color_codes("muted")
+sns.set_color_codes("bright")
 
 # plot the beginning of semester one results
 sns.barplot(x=merged.index, y="Transcription_begin", data=merged,
-            label="September, 2021", color="b")
+            label="September, 2021", color="r")
 
 # rotate xticks so they are readable
 plt.xticks(rotation=90)
