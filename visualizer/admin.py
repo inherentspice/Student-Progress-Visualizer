66268f3c014304f92f9107d459000200a46c1e2f
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-16 02:19:07 -0700

    added models to app

--- visualizer/admin.py
+++ visualizer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Class, Student, Grades
+
+admin.site.register(Class)
+admin.site.register(Student)
+admin.site.register(Grades)
--- visualizer/models.py
+++ visualizer/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Class(models.Model):
+    class_name = models.CharField(max_length=200)
+    grade = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.class_name + '(' + self.grade + ')'
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=200)
+    student_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.student_name
+
+class Grades(models.Model):
+    student_name = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    class Subjects(models.TextChoices):
+        sight_words = "Sight Words"
+        reading = "Reading"
+        math = "Math"
+        writing = "Writing"
+        phonics = "Phonics"
+
+    subject = models.CharField(
+        max_length=12,
+        choices=Subjects.choices,
+        default=Subjects.reading,
+        )
+    grades = models.PositiveIntegerField(default=50, validators=[MinValueValidator(0), MaxValueValidator(100)])
