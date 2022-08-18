452691be657a31a167638caad1e00122981e5764
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-18 01:44:56 -0700

    added basic representation and use of models in views

--- visualizer/urls.py
+++ visualizer/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<str:class_id>/', views.class_overview, name='class'),
+    path('<str:class_id>/grades', views.grades, name='grades'),
 ]
--- visualizer/views.py
+++ visualizer/views.py
@@ -1,6 +1,20 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from pydoc import classname
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
+from .models import Class, Student
 
 def index(request):
-    return HttpResponse("You made it to the visualizer.")
+    class_list = Class.objects.order_by('grade')
+    context = {
+        'class_list': class_list
+    }
+    return render(request, 'visualizer/index.html', context)
+
+def class_overview(request, class_id):
+    students = get_list_or_404(Student, pk=class_id)
+    return render(request, 'visualizer/class_overview.html', {'students': students})
+
+def grades(request, class_name):
+    response = "You're looking at the grades for class %s"
+    return HttpResponse(response % class_name)
