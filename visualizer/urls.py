7995d926c01ea34ed18830f7c924aef4892a1d81
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-21 00:02:24 -0700

    added form to accept classroom information

--- visualizer/urls.py
+++ visualizer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+appname = 'visualizer'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('class-registration/', views.class_registration, name='class-registration'),
     path('<str:class_id>/', views.class_overview, name='class'),
     path('<str:class_id>/grades', views.grades, name='grades'),
 ]
