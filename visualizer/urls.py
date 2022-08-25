c229fccce44986a8be255583359bbc130b46c3f3
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-25 02:17:20 -0700

    redirected grades post request back to classroom_overview, added field to grades model

--- visualizer/urls.py
+++ visualizer/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('classroom-registration/', views.classroom_registration, name='classroom-registration'),
     path('<int:classroom_id>/', views.classroom_overview, name='classroom'),
-    path('<int:classroom_id>/grades', views.grades, name='grades'),
+    path('<int:classroom_id>/<int:student_id>/grades/', views.grades, name='grades'),
     path('<int:classroom_id>/classroom-update', views.classroom_update, name='classroom-update')
 ]
