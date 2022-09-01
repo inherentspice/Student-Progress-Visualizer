f398ff9162503e8981051713acac870bbc165749
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-01 03:05:39 -0700

    used ajax and chart.js to create bar graph of student grades

--- visualizer/urls.py
+++ visualizer/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('<int:classroom_id>/<int:student_id>/student-update', views.student_update, name='student-update'),
     path('<int:classroom_id>/<int:student_id>/student-delete', views.student_delete, name='student-delete'),
     path('<int:classroom_id>/classroom-update', views.classroom_update, name='classroom-update'),
-    path('<int:classroom_id>/classroom-delete', views.classroom_delete, name='classroom-delete')
+    path('<int:classroom_id>/classroom-delete', views.classroom_delete, name='classroom-delete'),
+    path('<int:classroom_id>/<int:student_id>/chart/', views.charts, name='charts'),
+    path('<int:student_id>/grades-chart/', views.grades_chart, name='grades-chart'),
+
 ]
